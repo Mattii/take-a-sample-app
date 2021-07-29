256eb8cd07e5c58b2f00ec94de1b4ce9d3a3e0bc
Mattii <sliwowski.mateusz@wp.pl>
2021-07-29 15:00:04 +0200

    crop cards style tweeks

--- views/Home.js
+++ views/Home.js
@@ -9,27 +9,44 @@ export default {
             <login-baner v-else />
         </base-section>
         <base-section v-if="newSample.length > 0 && isToken" id="newSampleVarietyList">
-        <div class="sample-list-wrapper">
-            <h2>New Sample ({{ newSample.length }})</h2>
-            <ul class="sample-list">
-                <router-link
-                    v-for="item in newSample" 
-                    :key="item.id" :to="{name: 'details.show', params: {id: item.id}}">
-                    <li  class="sampleListItem">
-                        <div>
-                            <p class="varietySegment">{{ item.cropSegment }}</p>
-                            <h3 class="varietyName">{{ item.cropName.toLocaleUpperCase() }}</h3>
-                        </div>
-                        <p class="varietyPackingDate">Data pakowania: <span>{{ item.packingDate }}</span></p>
-                        <p class="varietyStock">{{ item.cropPacking }} nasion x {{ item.cropQuantity }} opakowania ( {{ +item.cropPacking * +item.cropQuantity }} nasoin )</p>
-                        <p class="remarkCounter" v-if="item.remarks && item.remarks.length > 0">
-                        there is {{ item.remarks.length }} remark
-                        </p>		
-                        <img :src="'./img/vegetables/'+ item.cropSegment +'.png'" alt="lt_butter_leaf" >
-                    </li>
-                </router-link>
-            </ul>
-        </div>
+            <div class="sample-list-wrapper">
+                <h2>New Sample ({{ newSample.length }})</h2>
+                <ul class="sample-list">
+                    <router-link
+                        v-for="item in newSample" 
+                        :key="item.id" :to="{name: 'details.show', params: {id: item.id}}">
+                        <li  class="sample-list-item">
+                            <div>
+                                <p class="variety-segment varietyLabel">{{ item.cropSegment }}</p>
+                                <h3 class="variety-name">{{ item.cropName.toLocaleUpperCase() }}</h3>
+                            </div>
+                            <div>
+                                <p class="varietyLabel">rozmiar pakowania</p> 
+                                <p class="variety-packing">{{ item.cropPacking }}</p>
+                            </div>
+                            <div>
+                                <p class="varietyLabel">ilość opakowań</p>
+                                <p class="variety-quantity">{{ item.cropQuantity }}</p>
+                            </div>
+                            <div>
+                                <p class="varietyLabel">ilość nasion</p>
+                                <p class="variety-stoc=quantity">{{ +item.cropPacking * +item.cropQuantity }}</p>
+                            </div>
+                            <div>
+                                <p class="varietyLabel">data pakowania</p>
+                                <p class="variety-packing-date">{{ item.packingDate }}</p>
+                            </div>
+                            <p class="remark-counter" v-if="item.remarks && item.remarks.length > 0">
+                            there is {{ item.remarks.length }} remark
+                            </p>
+                            <div>
+                                <img :src="'./img/vegetables/'+ item.cropSegment +'.png'" alt="lt_butter_leaf" >		
+                                <img :src="'./img/vegetables/'+ item.cropSegment +'.png'" alt="lt_butter_leaf" >
+                            </div>
+                        </li>
+                    </router-link>
+                </ul>
+            </div>
         </base-section>
 
         <base-section v-if="items.length > 0 && isToken" id="sampleVarietyList">
