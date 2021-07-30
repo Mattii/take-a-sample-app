9aa0d81c34360016b7ef452e60ed3ce90b1c9dfd
Mattii <sliwowski.mateusz@wp.pl>
2021-07-30 14:44:32 +0200

    addig not found page and page guard

--- router.js
+++ router.js
@@ -3,12 +3,20 @@ import Home from "./views/Home.js";
 const routes = [
     { path: '/', name: 'Home', component: Home },
     { path: '/login', name: 'login.user', component: () => import("./views/Login.js")},
-    { path: '/sample', name: 'Sample', component: () => import("./views/Sample.js") },
+    { path: '/sample', name: 'Sample', component: () => import("./views/Sample.js"), meta: { requiresAuth: false } },
     { path: '/details/:id', name: 'details.show', props: route => ({id: route.params.id}), component: () => import("./views/SampleDetails.js") },
+    { path: '/:pathMatch(.*)*', name: 'notfound', component: () => import('./views/NotFound.js') }
   ]
 const router = VueRouter.createRouter({
     history: VueRouter.createWebHistory(),
     routes, // short for `routes: routes`
   })
+
+router.beforeEach( (to, from) => {
+  // ...
+  if(to.meta.requiresAuth) {
+    return {path: '/login'}
+  }
+})
 
 export default router
---
+++ views/NotFound.js
@@ -0,0 +1,9 @@
+export default {
+    template: `
+        <div>
+            <base-section>
+                <h1>Page not found Sorry!</h1>
+            </base-section>
+        </div>
+    `,
+}
