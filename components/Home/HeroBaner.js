bfbf046917eda0327d3864149b0f92b526282fe6
Mattii <sliwowski.mateusz@wp.pl>
2021-08-20 14:30:00 +0200

    hero img added, some tweeks for proper displaying

--- components/Home/HeroBaner.js
+++ components/Home/HeroBaner.js
@@ -1,6 +1,12 @@
 export default {
     template: `
             <div class="hero-element" style="background-image: url();">
+                <img src="./img/vegetables/ogórek.png">
+                <img src="./img/vegetables/pomidor.png">
+                <img src="./img/vegetables/rzodkiewka.png">
+                <img src="./img/vegetables/sałata.png">
+                <img src="./img/vegetables/dynia-pirzmowa.png">
+                <img src="./img/vegetables/ogorek-pickling.png">
                 <h2>Predict the future and <br /><span>take a sample</span> <br /> of innovation</h2>
                 <router-link class="btn btn-to-action" :to="{name: 'samples'}">Take Some</router-link>
             </div>
